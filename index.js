5ef2a879a784be8d21e611408d482fd9e70fe448
Hershy Schwartz <hersheyschwartz1@gmail.com>
2026-01-04 05:30:13 +0200

    requirments completed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,9 +16,36 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let i = 0
+const shortenedURls = []
+
+app.post('/api/shorturl', function(req,res) {
+  let url = req.body.url
+  const regex = /^https?:\/\/\w+\.\w+$/
+  if (!regex.test(url)) {
+    res.json({ error: 'invalid url' })
+    return
+  } else {
+    let found = shortenedURls.find(route => route.original_url === url)
+    if (found) {
+      res.json(found)
+    } else {
+      const route = {original_url: url, short_url: ++i}
+      console.log(route, req.body)
+      shortenedURls.push(route)
+      res.json(route)
+    }
+  }
+});
+
+app.get(`/api/shorturl/:id`, function (req, res) {
+  let id = parseInt(req.params.id)
+  let route = shortenedURls.find(route => route.short_url === id)
+  if (route) {
+      res.redirect(route.original_url)
+  } else {
+    res.status(404).json({error: 'URL not found!'})
+  }
 });
 
 app.listen(port, function() {
